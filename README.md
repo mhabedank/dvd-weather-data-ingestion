82f0c59aa916fee3a9c9fd23c5a0a60973466c66
m.habedank <m.habedank@cartelsol.com>
2023-09-07 22:37:44 +0200

    Refactored code, added web api

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Einleitung
+# Datenarchitektur Wetterdaten
+
+## Einleitung
 
 Die Wetterdaten der Wetterstation liegen als Dateien auf einem Share des DWD vor.
 Dabei werden Daten wie Windgeschwindigkeit, Windrichtung etc. in einer Datei gespeichert.
@@ -74,3 +76,16 @@ und später zum Filtern verwendet.
 
 Um den ETL Prozess durchzuführen, müssen lediglich die Notebooks im Ordner `notebooks` ausgeführt in der
 Reihenfolge ausgeführt werden, in der sie nummeriert sind. Weitere Details stehen in diesen Dateien.
+
+Grob sind die Schritte wie folgt:  
+**1) Download der Rohdaten**: In den Notebooks 1 bis 3 werden die Rohdaten in Form von gezippten Dateien heruntergeladen.  
+**2) Extrahieren der Rohdaten**: Im Notebook 4 werden die Rohdaten extrahiert.  
+**3) Laden der Daten in Parquet-Dateien**: Im Notebook 5 werden die Daten Parquet-Dateien geladen, um zu für den 
+upload in die Datenbank zur Verfügung zu stellen.  
+**4)Laden der Daten in die Datenbank**: Im Notebook 6 werden die Daten in die Datenbank geladen.  
+**5) Zugriff auf die Daten**: Im Notebook 7 wird ein Beispiel für den Zugriff auf die Daten gezeigt.  
+
+# Web-API
+im Notebook 8 wird eine Web-API implementiert, die die Daten aus der Datenbank über das Web zur Verfügung stellt.
+Implementiert ist dabei eine abgespeckte RESTfulAPI, die lediglich lesenden Zugriff auf die Daten bietet
+und ein erweiterter Endpunkt, der es möglich macht, basierend auf einer Geoposition und einem Radius in KM nach Stationen zu suchen.
--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import peewee as orm
 import os
 from dotenv import load_dotenv
-from datetime import date
+import pandas as pd
 
 load_dotenv("../.env", override=True)
 
@@ -19,6 +19,33 @@ class Station(orm.Model):
     class Meta:
         table_name = "station"
         database = db
+
+    @classmethod
+    def get_as_dicts(cls):
+        return (
+            cls.select(
+                Station,
+                StationName.name,
+                StationLocation.latitude,
+                StationLocation.longitude,
+                StationLocation.altitude,
+            )
+            .join(
+                StationName,
+                on=(
+                    (StationName.station_id == Station.id)
+                    & StationName.to_date.is_null()
+                ),
+            )
+            .join(
+                StationLocation,
+                on=(
+                    (StationLocation.station_id == Station.id)
+                    & StationLocation.to_date.is_null()
+                ),
+            )
+            .dicts()
+        )
 
 
 class StationName(orm.Model):
@@ -46,10 +73,6 @@ class StationLocation(orm.Model):
         table_name = "station_location"
         database = db
 
-    # TODO: Berlin Brandenburg Query.
-    # TODO: Business Rule Check (From To Date)
-    # TODO: check for consistency with great expectations
-
 
 class Wind10Minutes(orm.Model):
     QUALITY_CHOICE = [
@@ -68,6 +91,50 @@ class Wind10Minutes(orm.Model):
     class Meta:
         table_name = "wind_10_minutes"
         database = db
+
+    @classmethod
+    def get_timeseries_by_station_name(
+        cls, station_name, start_date=None, end_date=None, as_dataframe=True
+    ):
+        if not end_date:
+            end_date = pd.Timestamp.now().date()
+        if not start_date:
+            start_date = end_date - pd.Timedelta(days=365)
+
+        join_predicates = (Wind10Minutes.station == StationLocation.station) & (
+            (
+                (StationLocation.from_date <= Wind10Minutes.timestamp)
+                & (Wind10Minutes.timestamp <= StationLocation.to_date)
+            )
+            | (
+                (StationLocation.to_date is None)
+                & (StationLocation.from_date <= Wind10Minutes.timestamp)
+            )
+        )
+
+        query = (
+            cls.select(
+                Wind10Minutes.timestamp,
+                StationLocation.latitude,
+                StationLocation.longitude,
+                Wind10Minutes.avg_speed,
+                Wind10Minutes.avg_direction,
+            )
+            .join(StationLocation, on=join_predicates)
+            .join(StationName, on=(Wind10Minutes.station == StationName.station))
+            .where(
+                (StationName.name == station_name)
+                & (start_date <= Wind10Minutes.timestamp)
+                & (Wind10Minutes.timestamp <= end_date)
+            )
+            .order_by(Wind10Minutes.timestamp)
+            .dicts()
+        )
+
+        if as_dataframe:
+            return pd.DataFrame(list(query))
+        else:
+            return list(query)
 
 
 def model_init():
